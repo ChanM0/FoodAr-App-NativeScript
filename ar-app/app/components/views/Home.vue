a7695a2d1ba31e6b3d1b4d7268944d30ec9291c1
Timothy Brumbaugh <timothy.brumbaugh@metalab.csun.edu>
2019-01-28 14:22:20 -0800

    here we go

---
+++ ar-app/app/components/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <page>
+      <font-awesome-icon icon="coffee"></font-awesome-icon>
+    </page>
+</template>
+
+<script>
+import master_navbar from "../MasterNavbar"
+export default {
+    name: "Home",
+    components: {
+        master_navbar,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
