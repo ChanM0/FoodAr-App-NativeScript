58d757208043a724bf82ac47db38a8a50f7a6ba1
Timothy Brumbaugh <timothy.brumbaugh@metalab.csun.edu>
2019-02-11 01:30:41 -0800

    working..

--- ar-app/app/components/MasterNavbar.vue
+++ ar-app/app/components/MasterNavbar.vue
@@ -1,27 +1,26 @@
 <template>
 
-    <GridLayout class="navBottom" height="50" width="100%" row="3" rows="auto"
-        columns="auto,auto,auto,auto, auto">
-        <GridLayout :class="selectedTab === 'scan' ? 'active' : ''" @tap="routeToTab('Scan')" rows="*,auto"
-            cols="auto" class="nav" col="0" row="0" width="20%">
-            <Image :class="selectedTab === 'scan' ? 'active' : ''" row="0" class="navIcon" :src="'~/assets/images/thermometer.png'" />
-            <Label row="1" v-show="selectedTab === 'scan'" text="scan" class="navText"></Label>
+    <GridLayout class="navBottom" height="60" width="100%" row="5" rows="auto"
+        columns="auto,auto,auto,auto,auto">
+        <GridLayout :class="selectedTab === 'Scan' ? 'active' : ''" @tap="routeToTab('Scan')" rows="*,auto" cols="auto" class="nav" col="0" row="0" width="20%">
+             <Label :class="selectedTab === 'Scan' ? 'active' : ''" class="fas" row="0" :text="'fa-qrcode' | fonticon" fontSize="24" ></Label>
+            <Label row="1" v-show="selectedTab === 'Scan'" text="Scan" class="navText"></Label>
         </GridLayout>
-        <GridLayout :class="selectedTab === 'profile' ? 'active' : ''" @tap="routeToTab('Profile')" rows="*,auto" cols="auto" class="nav" col="1" row="0" width="20%">
-            <Image :class="selectedTab === 'profile' ? 'active' : ''" row="0" class="navIcon" :src="'~/assets/images/calendar.png'" />
-            <Label row="1" v-show="selectedTab === 'profile'" text="profile" class="navText"></Label>
+        <GridLayout :class="selectedTab === 'Profile' ? 'active' : ''" @tap="routeToTab('Profile')" rows="*,auto" cols="auto" class="nav" col="1" row="0" width="20%">
+            <Label class="fas" row="0" :class="selectedTab === 'Profile' ? 'active' : ''" :text="'fa-user' | fonticon" fontSize="24" ></Label>
+            <Label row="1" v-show="selectedTab === 'Profile'" text="Profile" class="navText"></Label>
         </GridLayout>
-        <GridLayout :class="selectedTab == 'search' ? 'active' : ''" @tap="routeToTab('Search')" rows="*,auto" cols="auto" class="nav" col="2" row="0" width="20%">
-            <Image :class="selectedTab == 'search' ? 'active' : ''" row="0" class="navIcon" :src="'~/assets/images/home.png'" />
-            <Label row="1" v-show="selectedTab == 'search'" text="search" class="navText"></Label>
+        <GridLayout :class="selectedTab == 'Search' ? 'active' : ''" @tap="routeToTab('Search')" rows="*,auto" cols="auto" class="nav" col="2" row="0" width="20%">
+            <Label class="fas" :class="selectedTab === 'Search' ? 'active' : ''" row="0" :text="'fa-search' | fonticon" fontSize="24" ></Label>
+            <Label row="1" v-show="selectedTab == 'Search'" text="Search" class="navText"></Label>
         </GridLayout>
-        <GridLayout :class="selectedTab == 'bookmarks' ? 'active' : ''" @tap="routeToTab('Bookmarks')" rows="*,auto" cols="auto" class="nav" col="3" row="0" width="20%">
-            <Image :class="selectedTab == 'bookmarks' ? 'active' : ''" row="0" class="navIcon" :src="'~/assets/images/bell.png'" />
-            <Label row="1" v-show="selectedTab == 'bookmarks'" text="bookmarks" class="navText"></Label>
+        <GridLayout :class="selectedTab == 'Bookmarks' ? 'active' : ''" @tap="routeToTab('Bookmarks')" rows="*,auto" cols="auto" class="nav" col="3" row="0" width="20%">
+            <Label class="fas" row="0" :class="selectedTab === 'Bookmarks' ? 'active' : ''" :text="'fa-bookmark' | fonticon" fontSize="24" ></Label>
+            <Label row="1" v-show="selectedTab == 'Bookmarks'" text="Bookmarks" class="navText"></Label>
         </GridLayout>
-        <GridLayout :class="selectedTab == 'settings' ? 'active' : ''" @tap="routeToTab('Settings')" rows="*,auto" cols="auto" class="nav" col="4" row="0" width="20%">
-            <Image :class="selectedTab == 'settings' ? 'active' : ''" row="0" class="navIcon" :src="'~/assets/images/cog.png'" />
-            <Label row="1" v-show="selectedTab == 'settings'" text="settings" class="navText"></Label>
+        <GridLayout :class="selectedTab == 'Settings' ? 'active' : ''" @tap="routeToTab('Settings')" rows="*,auto" cols="auto" class="nav" col="4" row="0" width="20%">
+            <Label class="fas" :class="selectedTab === 'Settings' ? 'active' : ''" row="0" :text="'fa-cog' | fonticon" fontSize="24" ></Label>
+            <Label row="1" v-show="selectedTab == 'Settings'" text="Settings" class="navText"></Label>
         </GridLayout>
     </GridLayout>
 
