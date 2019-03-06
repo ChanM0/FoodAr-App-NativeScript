8f89dd563c4db36cf9a4d63b45c7545b3b06faa9
Timothy Brumbaugh <timothy.brumbaugh@metalab.csun.edu>
2019-03-05 23:21:04 -0800

    Test

--- ar-app/app/components/views/Home.vue
+++ ar-app/app/components/views/Home.vue
@@ -1,12 +1,49 @@
 <template>
   <GridLayout row="1">
-    <TextView text="Search Page" />
+    <ScrollView class="anim-images" orientation="horizontal">
+      <StackLayout orientation="horizontal" class="">
+          <GridLayout v-for="item in arCollection" rows="auto" columns="*">
+              <Image class="card-img-thumb" row="0" col="0" :src="item.imageSrc"
+                  stretch="aspectFill" />
+          </GridLayout>
+      </StackLayout>
+    </ScrollView>
   </GridLayout>
 </template>
 
 <script>
 export default {
-
+data() {
+    return {
+      arCollection: [
+        {
+          id: 1,
+          name: "Germany",
+          imageSrc: "https://play.nativescript.org/dist/assets/img/flags/de.png"
+        },
+        {
+          id: 2,
+          name: "Spain",
+          imageSrc: "https://play.nativescript.org/dist/assets/img/flags/es.png"
+        },
+        {
+          id: 3,
+          name: "Ethiopia",
+          imageSrc: "https://play.nativescript.org/dist/assets/img/flags/et.png"
+        },
+        {
+          id: 4,
+          name: "Croatia",
+          imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hr.png"
+        },
+        {
+          id: 5,
+          name: "Hungary",
+          imageSrc: "https://play.nativescript.org/dist/assets/img/flags/hu.png"
+        }
+      ]
+    };
+  },
 }
 </script>
 
