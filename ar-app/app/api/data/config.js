07c05aa04cac199d077ac2251ef454002776f6cd
Ryan Bernstein <ryanbbernstein@gmail.com>
2019-03-06 13:09:17 -0800

    finish up auth actions

--- ar-app/app/api/data/config.js
+++ ar-app/app/api/data/config.js
@@ -1,6 +1,5 @@
 export default {
-  API_URL:
-    "https://firestore.googleapis.com/v1beta1/projects/food-ar-8f4c0/databases/(default)/documents/",
+  API_URL: "https://firestore.googleapis.com/v1beta1/projects/food-ar-8f4c0/databases/(default)/documents/",
 
   MENUS: "menus",
   RESTAURANTS: "restuarants",
