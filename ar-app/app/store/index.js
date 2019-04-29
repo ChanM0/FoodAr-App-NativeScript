28f81e520dd68ba9b358887c3eca714a26c458b1
chanm0 <mikec201722@gmail.com>
2019-04-29 16:34:02 -0700

    updated store

--- ar-app/app/store/index.js
+++ ar-app/app/store/index.js
@@ -1,15 +1,17 @@
-import Vue from 'nativescript-vue'
-import Vuex from 'vuex'
+import Vue from "nativescript-vue";
+import Vuex from "vuex";
 
 //module imports
-import navigation from './modules/navigation'
+import navigation from "./modules/navigation";
+import home from "./modules/home";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {
-        navigation
-    },
-    // strict: debug,
-    // plugins: debug ? [createLogger()] : []
-})
+  modules: {
+    navigation,
+    home
+  }
+  // strict: debug,
+  // plugins: debug ? [createLogger()] : []
+});
